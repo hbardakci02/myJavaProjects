ae423fec6848055c03a37a9fc7eb78dd0e00c850
Hüseyin Bardakçı <hbardakci02@gmail.com>
2024-06-27 17:55:31 +0300

    COMMİT

---
+++ collections/HahSetTalat.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HahSetTalat {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<Integer>();
+        Set<Integer> mySwt = new HashSet<>();
+        mySwt.add(5);
+
+        mySwt.contains(5);
+    }
+
+}
